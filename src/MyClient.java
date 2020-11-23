cf0992abe46e870aa9f04c30788493dcc55ff01d
khardrix <carmenandryanhuffman@yahoo.com>
2020-11-22 22:52:29 -0500

    second commit

--- src/MyClient.java
+++ src/MyClient.java
@@ -1,80 +1,39 @@
 import java.io.*;
 import java.net.*;
-
+import java.util.Scanner;
 
 public class MyClient {
 
-//    public static void main(String[] args) {
-//        Socket toserversocket;  // This is the phone for communication with server
-//        int reply;       // ignore - for use in a later example
-//
-//        System.out.println("CLIENT is attempting connection....");
-//
-//        try {
-//            // connect to the address (in this case “localhost”) at door # 7788
-//            toserversocket = new Socket("localhost", 7788);
-//            System.out.println("CONNECTION HAS BEEN MADE");
-//        }
-//        catch (IOException e) {
-//            System.out.println(e);
-//            e.printStackTrace();
-//        }
-//    }
-//}
-
+    private static Scanner input = new Scanner(System.in);
     private static DataInputStream instream;
     private static DataOutputStream outstream;
-    public static Socket toserversocket = new Socket();     // This is the phone for communication with server
+    private static int row = -1, col = -1, serverRow = -1, serverCol = -1;
 
 
-    public static void main( String[] args)
-    {
+    public static void main(String[] args) {
 
-        int reply;                 // ignore - for use in a later example
-
-
-
-
-//        instream = new DataInputStream( toserversocket.getInputStream());
-//        outstream = new DataOutputStream( toserversocket.getOutputStream());
-//        PrintWriter   out = new PrintWriter(outstream, true);
-//        BufferedReader  in = new BufferedReader(new InputStreamReader(instream));
-
-
+        Socket toserversocket;  // This is the phone for communication with server
+        int reply;       // ignore - for use in a later example
 
         System.out.println("CLIENT is attempting connection....");
         try {
+            // connect to the address (in this case “localhost”) at door # 7788
+            toserversocket = new Socket("localhost", 7788);
+            System.out.println("CLIENT CONNECTION HAS BEEN MADE");
+
             instream = new DataInputStream(toserversocket.getInputStream());
             outstream = new DataOutputStream(toserversocket.getOutputStream());
             PrintWriter out = new PrintWriter(outstream, true);
             BufferedReader in = new BufferedReader(new InputStreamReader(instream));
 
-            // connect to the address (in this case “localhost”) at door # 7788
-            toserversocket = new Socket("localhost", 7788);
-            System.out.println("CONNECTION HAS BEEN MADE");
+            row = -1;
+            col = -1;
+
+
         }
         catch (IOException  e) {
             System.out.println(e);
-            e.printStackTrace();
         }
-
-
-        System.out.println("CLIENT is attempting connection....");
-        try {
-            instream = new DataInputStream(toserversocket.getInputStream());
-            outstream = new DataOutputStream(toserversocket.getOutputStream());
-            PrintWriter out = new PrintWriter(outstream, true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(instream));
-
-            // connect to the address (in this case “localhost”) at door # 4030
-            toserversocket = new Socket("localhost", 7788);
-            System.out.println("CONNECTION HAS BEEN MADE");
-        }
-        catch (IOException e) {
-            System.out.println(e);
-            e.printStackTrace();
-        }
-
-
     }
 }
+
